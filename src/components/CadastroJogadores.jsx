ec31e6d6ac1dfbd208cbf53eeb58cce17f0aa6c4
Gabriel Alves <gabriel@meucrediario.com.br>
2024-09-29 21:16:00 -0300

    adicionado botões de mais e menos jogadores, assim como a validação de número de jogadores

--- src/components/CadastroJogadores.jsx
+++ src/components/CadastroJogadores.jsx
@@ -1,12 +1,58 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './../styles/CadastroJogadores.scss';
 
 export function CadastroJogadores({ onGameStart }) {
     const [numJogadores, setNumJogadores] = useState(2);
     const [nomes, setNomes] = useState([]);
+    const [podeDiminuirJogador, setPodeDiminuirJogador] = useState(true);
+    const [podeAumentarJogador, setPodeAumentarJogador] = useState(true);
 
     const handleNumJogadoresChange = (e) => {
-        setNumJogadores(Number(e.target.value));
+        let valor = parseInt(e.target.value, 10);
+
+        // Permite valor vazio temporariamente, mas impede que seja menor que 1
+        if (
+            valor === '' ||
+            (parseInt(valor, 10) >= 1 && parseInt(valor, 10) <= 20)
+        ) {
+            setNumJogadores(valor);
+        }
+    };
+
+    const handleBlur = () => {
+        // Se o valor for vazio ao sair do campo, redefine para 1
+        if (numJogadores === '' || numJogadores < 1) {
+            setNumJogadores(1);
+        } else if (numJogadores > 20) {
+            setNumJogadores(20);
+        }
+    };
+
+    const handleMenosJogador = () => {
+        // Não diminuir jogador se só tiver um
+        if (numJogadores === 1) {
+            return;
+        }
+        setNumJogadores(numJogadores - 1);
+    };
+
+    useEffect(() => {
+        setPodeDiminuirJogador(true);
+        setPodeAumentarJogador(true);
+        if (numJogadores === 20) {
+            setPodeAumentarJogador(false);
+        }
+        if (numJogadores === 1) {
+            setPodeDiminuirJogador(false);
+        }
+    }, [numJogadores]);
+
+    const handleMaisJogador = () => {
+        // Não deixa passar de 20 jogadores
+        if (numJogadores === 20) {
+            return;
+        }
+        setNumJogadores(numJogadores + 1);
     };
 
     const handleNomesJogadores = (i, valor) => {
@@ -37,15 +83,33 @@ export function CadastroJogadores({ onGameStart }) {
     };
 
     return (
-        <div className="container">
-            <label>
-                Número de jogadores:
-                <input
-                    value={numJogadores}
-                    onChange={handleNumJogadoresChange}
-                    inputMode="numeric"
-                    className="quantidade-jogadores"
-                />
+        <div className="container holder">
+            <label className="quantidade-jogadores">
+                <div className="flex">
+                    <button
+                        className={`minus font-bold ${podeDiminuirJogador ? '' : 'desativado'}`}
+                        onClick={handleMenosJogador}
+                    >
+                        -
+                    </button>
+                    <input
+                        type="number"
+                        value={numJogadores >= 1 ? numJogadores : 1}
+                        onChange={handleNumJogadoresChange}
+                        onBlur={handleBlur}
+                        inputMode="numeric"
+                        className="font-bold"
+                        min="1"
+                        max="20"
+                    />
+                    <button
+                        className={`plus font-bold ${podeAumentarJogador ? '' : 'desativado'}`}
+                        onClick={handleMaisJogador}
+                    >
+                        +
+                    </button>
+                </div>
+                jogadores
             </label>
             {Array.from({ length: numJogadores }, (_, i) => (
                 <input
