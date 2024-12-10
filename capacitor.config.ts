83b929fa05bb84aec9e49382629eaf7025366061
Gabriel Alves <gabriel@meucrediario.com.br>
2024-12-10 13:42:28 -0300

    install capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'com.marcadorgeneral.app',
+    appName: 'marcador-general',
+    webDir: 'dist',
+};
+
+export default config;
